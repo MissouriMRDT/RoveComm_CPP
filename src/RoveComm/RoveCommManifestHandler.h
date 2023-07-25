2a8c020389112af805fa0062af22a6d7b9f84de0
Eli Byrd <edbgkk@mst.edu>
2023-07-24 20:04:45 -0500

    Add existing files back in

---
+++ src/RoveComm/RoveCommManifestHandler.h
@@ -0,0 +1,71 @@
+/******************************************************************************
+ * @brief Defines the RoveCommManifestHandler class.
+ *
+ * @file RoveCommManifestHandler.h
+ * @author Eli Byrd (edbgkk@mst.edu), ClayJay3 (claytonraycowen@gmail.com)
+ * @date 2023-06-20
+ *
+ * @copyright Copyright MRDT 2023 - All Rights Reserved
+ ******************************************************************************/
+
+#include "../../../../../src/AutonomyGlobals.h"
+#include <array>
+#include <boost/property_tree/json_parser.hpp>
+#include <boost/property_tree/ptree.hpp>
+#include <string>
+#include <vector>
+
+#define ROVECOMM_MANIFEST_FILENAME "../Source/Autonomy/Networking/Manifest.json"
+
+#ifndef ROVECOMMMANIFESTHANDLER_H
+#define ROVECOMMMANIFESTHANDLER_H
+
+enum RoveCommManifestIdentifiers
+{
+    RCMI_CORE,
+    RCMI_NAV
+};
+
+enum RoveCommManifestIntegers
+{
+    RCMI_DATA_ID,
+    RCMI_DATA_COUNT
+};
+
+enum RoveCommManifestStrings
+{
+    RCMSZ_NAME,
+    RCMSZ_DATA_TYPE
+};
+
+struct RoveCommBoardCommand
+{
+        std::string szName;
+        int iDataId;
+        std::string szDataType;
+        int iDataCount;
+};
+
+struct RoveCommBoard
+{
+        std::string szIPAddress;
+        std::vector<RoveCommBoardCommand> vCommands;
+        std::vector<RoveCommBoardCommand> vTelemetry;
+};
+
+class RoveCommManifestHandler
+{
+    public:
+        RoveCommManifestHandler();
+        ~RoveCommManifestHandler();
+
+        void SetupBoard(RoveCommManifestIdentifiers eValue);
+
+        std::string GetIPAddress(RoveCommManifestIdentifiers eIdentifier) const;
+
+    private:
+        RoveCommBoard pCoreBoard;
+        RoveCommBoard pNavBoard;
+};
+
+#endif    // ROVECOMMMANIFESTHANDLER_H
