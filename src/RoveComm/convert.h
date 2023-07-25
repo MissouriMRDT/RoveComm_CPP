393a596fd64903687c87f5f75d38115edc0ea1bf
Eli Byrd <edbgkk@mst.edu>
2023-07-24 19:55:29 -0500

    Remove Test Files

--- src/RoveComm/convert.h
+++
@@ -1,6 +0,0 @@
-#include "../../../../../src/AutonomyGlobals.h"
-
-float convert_celsius_to_fahrenheit(float celsius);
-float convert_fahrenheit_to_celsius(float fahrenheit);
-float convert_kilometers_to_miles(float kilometers);
-float convert_miles_to_kilometers(float miles);
