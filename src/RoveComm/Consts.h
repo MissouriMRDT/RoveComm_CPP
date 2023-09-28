ebc510f7a4c2e15e4bab47e46ba9e481bad63282
Vrotki <51521287+Vrotki@users.noreply.github.com>
2023-09-28 18:45:31 -0500

    Test initial commit

--- src/RoveComm/Consts.h
+++ src/RoveComm/Consts.h
@@ -16,4 +16,17 @@
 #define ROVECOMM_PACKET_HEADER_SIZE           6
 #define ROVECOMM_VERSION                      3
 
+#define ROVECOMM_UDP_PORT                     11000
+/*
+const int ROVECOMM_UDP_PORT = 11000;
+const int ROVECOMM_TCP_PORT = 12000;
+const int ROVECOMM_VERSION = 3;
+const std::string ROVECOMM_HEADER_FORMAT = ">BHHB";
+
+const int ROVECOMM_PING_REQUEST = 1;
+const int ROVECOMM_PING_REPLY = 2;
+const int ROVECOMM_SUBSCRIBE_REQUEST = 3;
+const int ROVECOMM_UNSUBSCRIBE_REQUEST = 4;
+const int ROVECOMM_INCOMPATIBLE_VERSION = 5;
+*/
 #endif    // CONSTS_H
--- src/RoveComm/RoveComm.h
+++ src/RoveComm/RoveComm.h
@@ -11,4 +11,53 @@
 #ifndef ROVECOMM_H
 #define ROVECOMM_H
 
+#include "RoveComm.hpp"
+#include "Consts.h"
+#include <map>
+#include "string"
+
+/*
+        self.data_id = data_id
+        self.data_type = data_type
+        self.data_count = len(data)
+        self.data = data
+        # If the ip is less than 4 chars we know it's the 4th octet, otherwise it's the full IP
+        # in case of empty IP default to unknow IP
+        if ip_octet_4 != "" and len(ip_octet_4) < 4:
+            self.ip_address = ("192.168.1." + ip_octet_4, port)
+        elif ip_octet_4 != "" and len(ip_octet_4) >= 4:
+            self.ip_address = (ip_octet_4, port)
+        else:
+            self.ip_address = ("0.0.0.0", 0)
+        return
+*/
+
+class RoveCommPacket
+/*
+    The RoveComm packet is the encapsulation of a message sent across the rover
+    network that can be parsed by all rover computing systems.
+
+    A RoveComm Packet contains:
+        - A data id
+        - A data type
+        - The number of data entries (data count)
+        - The data itself
+
+    The autonomy implementation also includes the remote ip of the sender.
+
+    Methods:
+    --------
+        SetIp(ip, port):
+            Sets packet's IP to address parameter
+        print():
+            Prints the packet'c contents
+*/
+{
+public:
+	RoveCommPacket(int data_id=0, char data_type='b', int data[], std::string ip="", int port=ROVECOMM_UDP_PORT);
+
+};
+
+std::map<std::string, std::string> get_manifest(std::string path="");
+
 #endif    // ROVECOMM_H
