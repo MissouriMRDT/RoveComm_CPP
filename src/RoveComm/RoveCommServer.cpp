497710dbf8c11d0296a54695015a1d9913d947fc
OcelotEmpire <hobbz.pi@gmail.com>
2023-12-21 05:23:39 +0000

    moved boilerplate to RoveCommHelpers.h and removed multithreading so I can implement singlethreading first.

--- src/RoveComm/RoveCommServer.cpp
+++ src/RoveComm/RoveCommServer.cpp
@@ -9,31 +9,3 @@
  ******************************************************************************/
 
 #include "RoveCommServer.h"
-
-std::ostream& operator<<(std::ostream& out, const RoveCommAddress& address)
-{
-    out << address.m_cOctants[0] << '.';
-    out << address.m_cOctants[1] << '.';
-    out << address.m_cOctants[2] << '.';
-    out << address.m_cOctants[3] << ':' << address.m_unPort;
-    return out;
-};
-
-bool operator==(RoveCommAddress& address, RoveCommAddress& other)
-{
-    bool bSameIp = true;
-    for (int i = 0; i < 4; i++)
-    {
-        if (address.m_cOctants[i] != other.m_cOctants[i])
-        {
-            bSameIp = false;
-            break;
-        }
-    }
-    return address.m_unPort == other.m_unPort && bSameIp;
-}
-
-bool operator!=(RoveCommAddress& address, RoveCommAddress& other)
-{
-    return !(address == other);
-}
