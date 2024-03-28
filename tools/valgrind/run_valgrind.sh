4e291efa2bcc940a56c57847258ca7dc21b291e5
Eli Byrd <edbgkk@mst.edu>
2024-03-28 18:53:32 +0000

    Update Valgrid Script

--- tools/valgrind/run_valgrind.sh
+++ tools/valgrind/run_valgrind.sh
@@ -20,9 +20,11 @@ script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 supp_files=$(find "$script_dir" -maxdepth 1 -type f -name "*.supp")
 
 # Construct the Valgrind command
-valgrind_cmd="valgrind -s --leak-check=yes --show-leak-kinds=all "
+valgrind_cmd="valgrind -s --leak-check=yes --show-leak-kinds=all --track-origins=yes "
 if [ "$1" = "GitHub-Action" ]; then
   valgrind_cmd+=" --log-file=/opt/RoveComm_CPP/tools/valgrind/valgrind.rpt"
+else
+  valgrind_cmd+=" --log-file=/workspaces/RoveComm_CPP/tools/valgrind/valgrind.rpt"
 fi
 for supp_file in $supp_files; do
   valgrind_cmd+=" --suppressions=$supp_file"
@@ -30,4 +32,12 @@ done
 valgrind_cmd+=" $executable_path"
 
 # Run the Valgrind command
-eval $valgrind_cmd
+timeout --signal=SIGINT 60s $valgrind_cmd
+
+exit_code=$?
+
+if [ $exit_code -eq 124 ] || [ $exit_code -eq 0 ]; then
+    exit 0
+else
+    exit 1
+fi
